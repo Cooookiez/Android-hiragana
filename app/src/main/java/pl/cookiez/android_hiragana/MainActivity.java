f4b1199fa24445ecbce9a83874e4c69bdfd45600
Unknown <kukizk@gmail.com>
2018-02-28 16:52:00 +0100

    dodanie

--- app/src/main/java/pl/cookiez/android_hiragana/MainActivity.java
+++ app/src/main/java/pl/cookiez/android_hiragana/MainActivity.java
@@ -2,12 +2,37 @@ package pl.cookiez.android_hiragana;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btt_00;
+    Button btt_01;
+    Button btt_02;
+    Button btt_10;
+    Button btt_11;
+    Button btt_12;
+    Button btt_20;
+    Button btt_21;
+    Button btt_22;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btt_00 = (Button)findViewById(R.id.btt_00);
+        btt_01 = (Button)findViewById(R.id.btt_01);
+        btt_02 = (Button)findViewById(R.id.btt_02);
+        btt_10 = (Button)findViewById(R.id.btt_10);
+        btt_11 = (Button)findViewById(R.id.btt_11);
+        btt_12 = (Button)findViewById(R.id.btt_12);
+        btt_20 = (Button)findViewById(R.id.btt_20);
+        btt_21 = (Button)findViewById(R.id.btt_21);
+        btt_22 = (Button)findViewById(R.id.btt_22);
+
+
+        
+
     }
 }
