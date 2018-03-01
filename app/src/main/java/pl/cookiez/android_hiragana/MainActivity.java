ff810062baf5f4527d9e9bd5b79a532326af0161
Unknown <kukizk@gmail.com>
2018-03-01 23:16:00 +0100

    Losuje głowne "pytanie"

--- app/src/main/java/pl/cookiez/android_hiragana/MainActivity.java
+++ app/src/main/java/pl/cookiez/android_hiragana/MainActivity.java
@@ -2,24 +2,61 @@ package pl.cookiez.android_hiragana;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Map<String, String> hira = new HashMap<String, String>();
 
-    Button btt_00;
-    Button btt_01;
-    Button btt_02;
-    Button btt_10;
-    Button btt_11;
-    Button btt_12;
-    Button btt_20;
-    Button btt_21;
-    Button btt_22;
+    String[] btts = new String[9];
+
+    Button btt_1;
+    Button btt_2;
+    Button btt_3;
+    Button btt_4;
+    Button btt_5;
+    Button btt_6;
+    Button btt_7;
+    Button btt_8;
+    Button btt_9;
+    Button btt_next;
+
+    TextView tv_what_is_it;
+
+    Random rand;
+
+    String what_is_hira;
+    String what_is_lati;
+
+    public void losuj(){
+
+        Log.i("losuj", "start");
+
+        int hira_size = hira.size();
+        int nth;
+        boolean jestPowturzenie;
+
+        //odpowiedz
+        nth = rand.nextInt(hira_size);
+        what_is_hira = (new ArrayList<String>(hira.values())).get(nth);
+        what_is_lati = (new ArrayList<String>(hira.keySet())).get(nth);
+        Log.i("what_is_hira", String.valueOf(what_is_hira));
+        Log.i("what_is_lati", String.valueOf(what_is_lati));
+        tv_what_is_it.setText(what_is_hira);
+        btts[0] = what_is_lati;
+
+        //guziki
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,71 +64,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         hira.put("a", "あ");
-        hira.put("i", "dada");
-        hira.put("u", "あ");
-        hira.put("e", "あ");
-        hira.put("o", "あ");
+        hira.put("i", "い");
+        hira.put("u", "う");
+        hira.put("e", "え");
+        hira.put("o", "お");
 
-        hira.put("ka", "あ");
-        hira.put("ki", "あ");
-        hira.put("ku", "あ");
-        hira.put("ke", "あ");
-        hira.put("ko", "あ");
+        hira.put("ka", "か");
+        hira.put("ki", "き");
+        hira.put("ku", "く");
+        hira.put("ke", "け");
+        hira.put("ko", "こ");
 
-        hira.put("sa", "あ");
-        hira.put("si", "あ");
-        hira.put("su", "あ");
-        hira.put("se", "あ");
-        hira.put("so", "あ");
+        hira.put("sa", "さ");
+        hira.put("si", "し");
+        hira.put("su", "す");
+        hira.put("se", "せ");
+        hira.put("so", "そ");
 
-        hira.put("ta", "あ");
-        hira.put("ti", "あ");
-        hira.put("tu", "あ");
-        hira.put("te", "あ");
-        hira.put("to", "あ");
+        hira.put("ta", "た");
+        hira.put("ti", "ち");
+        hira.put("tu", "つ");
+        hira.put("te", "て");
+        hira.put("to", "と");
 
-        hira.put("na", "あ");
-        hira.put("ni", "あ");
-        hira.put("nu", "あ");
-        hira.put("ne", "あ");
-        hira.put("no", "あ");
+        hira.put("na", "な");
+        hira.put("ni", "に");
+        hira.put("nu", "ぬ");
+        hira.put("ne", "ね");
+        hira.put("no", "の");
 
-        hira.put("ha", "あ");
-        hira.put("hi", "あ");
-        hira.put("hu", "あ");
-        hira.put("he", "あ");
-        hira.put("ho", "あ");
+        hira.put("ha", "は");
+        hira.put("hi", "ひ");
+        hira.put("hu", "ふ");
+        hira.put("he", "へ");
+        hira.put("ho", "ほ");
 
-        hira.put("ma", "あ");
-        hira.put("mi", "あ");
-        hira.put("mu", "あ");
-        hira.put("me", "あ");
-        hira.put("mo", "あ");
+        hira.put("ma", "ま");
+        hira.put("mi", "み");
+        hira.put("mu", "む");
+        hira.put("me", "め");
+        hira.put("mo", "も");
 
-        hira.put("ya", "あ");
-        hira.put("yu", "あ");
-        hira.put("yo", "あ");
+        hira.put("ya", "や");
+        hira.put("yu", "ゆ");
+        hira.put("yo", "よ");
 
-        hira.put("ra", "あ");
-        hira.put("ri", "あ");
-        hira.put("ru", "あ");
-        hira.put("re", "あ");
-        hira.put("ro", "あ");
+        hira.put("ra", "ら");
+        hira.put("ri", "り");
+        hira.put("ru", "る");
+        hira.put("re", "れ");
+        hira.put("ro", "ろ");
 
-        hira.put("wa", "あ");
-        hira.put("wi", "あ");
-        hira.put("we", "あ");
-        hira.put("wo", "あ");
+        hira.put("wa", "わ");
+        hira.put("wo", "を");
 
-        btt_00 = (Button)findViewById(R.id.btt_00);
-        btt_01 = (Button)findViewById(R.id.btt_01);
-        btt_02 = (Button)findViewById(R.id.btt_02);
-        btt_10 = (Button)findViewById(R.id.btt_10);
-        btt_11 = (Button)findViewById(R.id.btt_11);
-        btt_12 = (Button)findViewById(R.id.btt_12);
-        btt_20 = (Button)findViewById(R.id.btt_20);
-        btt_21 = (Button)findViewById(R.id.btt_21);
-        btt_22 = (Button)findViewById(R.id.btt_22);
+        btt_1 = (Button)findViewById(R.id.btt_1);
+        btt_2 = (Button)findViewById(R.id.btt_2);
+        btt_3 = (Button)findViewById(R.id.btt_3);
+        btt_4 = (Button)findViewById(R.id.btt_4);
+        btt_5 = (Button)findViewById(R.id.btt_5);
+        btt_6 = (Button)findViewById(R.id.btt_6);
+        btt_7 = (Button)findViewById(R.id.btt_7);
+        btt_8 = (Button)findViewById(R.id.btt_8);
+        btt_9 = (Button)findViewById(R.id.btt_9);
+
+        btt_next = (Button)findViewById(R.id.btt_next);
+
+        tv_what_is_it = (TextView)findViewById(R.id.tv_what_is_it);
+
+        rand = new Random();
+
+        btt_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("btt next", "start");
+                losuj();
+            }
+        });
 
     }
 }
