cc2ec4e8dc80f9191f87a79a75b3da29f94ae6bd
PC-MASTERRACE\kukiz <kukizk@gmail.com>
2018-03-26 19:18:01 +0200

    menu i przejscie do okna z grą

---
+++ app/src/main/java/pl/cookiez/android_hiragana/Chars.java
@@ -0,0 +1,74 @@
+package pl.cookiez.android_hiragana;
+
+/**
+ * Created by kukiz on 26.03.18.
+ */
+
+public class Chars{
+
+    public int hmChars = 48;
+    public String[] lati = new String[hmChars];
+    public  String[] hira = new String[hmChars];
+
+    public Chars(){
+        int i = 0;
+        lati[i] = "a";     hira[i]="あ";   i++;
+        lati[i] = "i";     hira[i]="い";   i++;
+        lati[i] = "u";     hira[i]="う";   i++;
+        lati[i] = "e";     hira[i]="え";   i++;
+        lati[i] = "o";     hira[i]="お";   i++;
+
+        lati[i] = "ka";    hira[i]="か";   i++;
+        lati[i] = "ki";    hira[i]="き";   i++;
+        lati[i] = "ku";    hira[i]="く";   i++;
+        lati[i] = "ke";    hira[i]="け";   i++;
+        lati[i] = "ko";    hira[i]="こ";   i++;
+
+        lati[i] = "sa";    hira[i]="さ";   i++;
+        lati[i] = "si";    hira[i]="し";   i++;
+        lati[i] = "su";    hira[i]="す";   i++;
+        lati[i] = "se";    hira[i]="せ";   i++;
+        lati[i] = "so";    hira[i]="そ";   i++;
+
+        lati[i] = "ta";    hira[i]="た";   i++;
+        lati[i] = "ti";    hira[i]="ち";   i++;
+        lati[i] = "tu";    hira[i]="つ";   i++;
+        lati[i] = "te";    hira[i]="て";   i++;
+        lati[i] = "to";    hira[i]="と";   i++;
+
+        lati[i] = "na";    hira[i]="な";   i++;
+        lati[i] = "ni";    hira[i]="に";   i++;
+        lati[i] = "nu";    hira[i]="ぬ";   i++;
+        lati[i] = "ne";    hira[i]="ね";   i++;
+        lati[i] = "no";    hira[i]="の";   i++;
+
+        lati[i] = "ha";    hira[i]="は";   i++;
+        lati[i] = "hi";    hira[i]="ひ";   i++;
+        lati[i] = "hu";    hira[i]="ふ";   i++;
+        lati[i] = "he";    hira[i]="へ";   i++;
+        lati[i] = "ho";    hira[i]="ほ";   i++;
+
+        lati[i] = "ma";    hira[i]="ま";   i++;
+        lati[i] = "mi";    hira[i]="み";   i++;
+        lati[i] = "mu";    hira[i]="む";   i++;
+        lati[i] = "me";    hira[i]="め";   i++;
+        lati[i] = "mo";    hira[i]="も";   i++;
+
+        lati[i] = "ya";    hira[i]="や";   i++;
+        lati[i] = "yu";    hira[i]="ゆ";   i++;
+        lati[i] = "yo";    hira[i]="よ";   i++;
+
+        lati[i] = "ra";    hira[i]="ら";   i++;
+        lati[i] = "ri";    hira[i]="り";   i++;
+        lati[i] = "ru";    hira[i]="る";   i++;
+        lati[i] = "re";    hira[i]="れ";   i++;
+        lati[i] = "ro";    hira[i]="ろ";   i++;
+
+        lati[i] = "wa";    hira[i]="わ";   i++;
+        lati[i] = "wi";    hira[i]="ゐ";   i++;
+        lati[i] = "we";    hira[i]="ゑ";   i++;
+        lati[i] = "wo";    hira[i]="を";   i++;
+        lati[i] = "nn";    hira[i]="ん";   i++;
+    }
+
+}
